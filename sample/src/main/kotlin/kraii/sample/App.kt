5af117ab37f04c1a61a5396191e9a49063341749
Johannes Barop <johannes.barop@codecentric.de>
2022-11-22 15:01:56 +0100

    added README

--- sample/src/main/kotlin/kraii/sample/App.kt
+++ sample/src/main/kotlin/kraii/sample/App.kt
@@ -3,6 +3,10 @@ package kraii.sample
 import kotlin.io.path.createTempFile
 import kotlin.io.path.deleteExisting
 
+/**
+ * A temporary file as an example for a non-heap
+ * resource.
+ */
 class ExternalResource : AutoCloseable {
 
   private val tempFile = createTempFile()
@@ -11,20 +15,31 @@ class ExternalResource : AutoCloseable {
     println("$tempFile has been created.")
   }
 
-  override fun close(){
+  /**
+   * The implementation of [AutoCloseable.close] is
+   * responsible for freeing the resource. In this
+   * case the file needs to be removed.
+   */
+  override fun close() {
     tempFile.deleteExisting()
     println("$tempFile has been deleted.")
   }
 }
 
+/**
+ * An 'complex' object which uses other resources.
+ *
+ * It extends the [AutoCloseable] interface, but does
+ * implement [AutoCloseable.close]. This is done
+ * automatically by the compiler plugin.
+ */
 class ResourceManager : AutoCloseable {
   private val firstResource = ExternalResource()
   private val secondResource = ExternalResource()
 }
 
-
 fun main() {
   ResourceManager().use {
-    println("Hello world from Source Code!")
+    println("Hello World!")
   }
 }
