4dcf83828d035b594c3854d830100229ce15ecd0
Johannes Barop <johannes.barop@codecentric.de>
2022-11-28 16:56:55 +0100

    simplify the composite build of the example

--- sample/settings.gradle.kts
+++ sample/settings.gradle.kts
@@ -1,8 +1,3 @@
 rootProject.name = "sample"
 
-includeBuild("../kraii-gradle-plugin")
-includeBuild("../kraii-compiler-plugin") {
-  dependencySubstitution {
-    substitute(module("kraii:kraii-compiler-plugin")).using(project(":"))
-  }
-}
+includeBuild("../")
