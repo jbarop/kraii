a8c4ffa35fcb4591ed8a6dfbd143e81bdae058ca
Johannes Barop <johannes.barop@codecentric.de>
2023-03-07 23:47:05 +0100

    upgrade dependencies

--- kraii-compiler-plugin/build.gradle.kts
+++ kraii-compiler-plugin/build.gradle.kts
@@ -6,10 +6,10 @@ dependencies {
   implementation("org.jetbrains.kotlin:kotlin-compiler-embeddable")
   implementation(project(":kraii-api"))
 
-  testImplementation(platform("org.junit:junit-bom:5.9.1"))
+  testImplementation(platform("org.junit:junit-bom:5.9.2"))
   testImplementation("org.junit.jupiter:junit-jupiter")
-  testImplementation("org.assertj:assertj-core:3.23.1")
-  testImplementation("com.google.code.gson:gson:2.10")
+  testImplementation("org.assertj:assertj-core:3.24.2")
+  testImplementation("com.google.code.gson:gson:2.10.1")
 }
 
 tasks.test {
