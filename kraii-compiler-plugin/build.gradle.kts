61492a8666210f51326a877570e4db971d8c0b98
Johannes Barop <johannes.barop@codecentric.de>
2023-11-29 09:49:37 +0100

    update gradle to 8.4 - fixed deprecation warning

--- kraii-compiler-plugin/build.gradle.kts
+++ kraii-compiler-plugin/build.gradle.kts
@@ -8,6 +8,7 @@ dependencies {
 
   testImplementation(platform("org.junit:junit-bom:5.9.2"))
   testImplementation("org.junit.jupiter:junit-jupiter")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
   testImplementation("org.assertj:assertj-core:3.24.2")
   testImplementation("com.google.code.gson:gson:2.10.1")
 }
