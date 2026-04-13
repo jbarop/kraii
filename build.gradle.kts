ec66ab25c2b6f651da86eb63268d8258ee8fdf6a
Johannes Barop <johannes.barop@codecentric.de>
2026-04-13 10:43:54 +0200

    Configure ktlint engine 1.8.0 consistently across all modules

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,13 @@ allprojects {
 plugins {
   // Applying external plugins with same version to subprojects
   alias(libs.plugins.kotlin.jvm) apply false
+  alias(libs.plugins.ktlint) apply false
+}
+
+subprojects {
+  pluginManager.withPlugin("org.jlleitschuh.gradle.ktlint") {
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+      version.set(rootProject.libs.versions.ktlint.engine)
+    }
+  }
 }
